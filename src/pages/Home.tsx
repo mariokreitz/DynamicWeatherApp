45bd234e8859cbb9f86999687f56c5a69e5ccdc8
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-16 16:08:16 +0100

    feat: add geolocation functionality to set city based on user's location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { WeatherContext } from "../context/WeatherContext";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const weatherContext = useContext(WeatherContext);
@@ -13,8 +15,26 @@ const Home = () => {
     setCity(e.target.value);
   };
 
+  const handleGeolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const geoCity = `${latitude},${longitude}`;
+          setCity(geoCity);
+        },
+        (error) => {
+          alert("Unable to retrieve your location");
+          console.error(error);
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen px-4">
+    <div className="flex flex-col items-center justify-center  px-4">
       <h1 className="text-4xl font-bold mb-6 text-center">Dynamic Weather App 🌤</h1>
       <input
         type="text"
@@ -23,9 +43,17 @@ const Home = () => {
         onChange={handleCityChange}
         className="border border-gray-300 dark:border-gray-700 px-4 py-2 rounded-lg mb-4 w-64 text-center bg-gray-50 dark:bg-gray-800"
       />
-      <Link to={`/city/${city}`} className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 rounded-lg">
-        Check Weather
-      </Link>
+      <div className="mb-4">
+        <Link to={`/city/${city}`} className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-6 py-2 rounded-lg">
+          Check Weather
+        </Link>
+      </div>
+      <button
+        onClick={handleGeolocation}
+        className="bg-green-500 hover:bg-green-600 text-white font-semibold px-6 py-2 rounded-lg flex items-center">
+        <FontAwesomeIcon icon={faLocationArrow} className="mr-2" />
+        Use My Location
+      </button>
     </div>
   );
 };
