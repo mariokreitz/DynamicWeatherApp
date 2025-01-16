aef0b282b4e3dae474310b7988bfbb563a16f764
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-16 16:10:12 +0100

    feat: wrap app in ThemeProvider and add Footer component for layout consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { WeatherProvider } from "./context/WeatherContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CityWeather from "./pages/CityWeather";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <WeatherProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/city/:cityName" element={<CityWeather />} />
-          </Routes>
-        </div>
-      </Router>
+      <ThemeProvider>
+        <Router>
+          <div className="min-h-screen flex flex-col justify-between bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/city/:cityName" element={<CityWeather />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </ThemeProvider>
     </WeatherProvider>
   );
 };
