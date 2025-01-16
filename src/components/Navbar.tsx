fbac47d24aeb70970eb4c38d803446d6227f440f
Mario Kreitz <Mario.Kreitz@web.de>
2025-01-16 16:10:42 +0100

    feat: add dark/light mode toggler to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,18 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../context/ThemeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const themeContext = useContext(ThemeContext);
+
+  if (!themeContext) return null;
+
+  const { isDarkMode, toggleTheme } = themeContext;
+
   return (
-    <nav className="bg-gray-800 text-white py-4 px-6 shadow-md">
+    <nav className="bg-gray-800 dark:bg-gray-900 text-white py-4 px-6 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-xl font-bold">
           🌦 WeatherApp
@@ -12,6 +22,9 @@ const Navbar = () => {
             Home
           </Link>
         </div>
+        <button onClick={toggleTheme} className="text-white">
+          <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
+        </button>
       </div>
     </nav>
   );
